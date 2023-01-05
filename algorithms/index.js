de389c51289ad291437a5e1c5e82916bf2e7e355
sal <salgarcia940@gmail.com>
2023-01-04 22:25:57 -0800

    some comments added

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,8 +1,8 @@
 function twoSum(nums,target){
   for(let i=0;i<nums.length;i++){
-    for(let j = i+1;j<nums.length;j++){
+    for(let j = i+1;j<nums.length;j++){ //i+1 rather than simply 1 to avoid j and i from landing on the same value twice
       if(nums[i]+nums[j]===target){
-        return [i,j]
+        return [i, j]//i and j only so that it grabs the indexes not the values
       }
     }
   }
