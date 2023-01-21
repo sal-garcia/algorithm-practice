8304a7ae61ceab3de2d444e9560e926a8dee10a9
sal <salgarcia940@gmail.com>
2023-01-21 01:08:03 -0800

    completed summation

--- sql-practice/index.sql
+++ sql-practice/index.sql
@@ -1,4 +1,6 @@
 --Simple Join
+--Assuming there are two tables(product and companies) with matching ID's columns
+--
 SELECT p.*,
        c.name AS company_name
   FROM products as p,
@@ -7,3 +9,12 @@ SELECT p.*,
 --the product id
 --could also be done with join
 --Simple Join
+
+--SUMMATION
+--Write a program that finds the summation of every number from 1 to num.
+--The number will always be a positive integer greater than 0.
+--assuming the column is n and the table is named kata
+--8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+select n, (n * (1 + n)) / 2 AS res
+from kata
+--SUMMATION
