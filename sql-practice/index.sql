8d42c3933856038ab00f8f61fbcfbd7c4273a011
sal <salgarcia940@gmail.com>
2023-01-26 17:09:06 -0800

    updated algorithm to be easier to read

--- sql-practice/index.sql
+++ sql-practice/index.sql
@@ -1,9 +1,8 @@
 --Simple Join
-SELECT p.*,
-       c.name AS company_name
-  FROM products as p,
-       companies as c
- WHERE c.id = p.company_id --assuming that company id matches
+select products.*, companies.name AS company_name
+FROM products
+JOIN companies ON companies.id=products.company_id
+--assuming that company id matches
 --the product id
 --could also be done with join
 --Simple Join
