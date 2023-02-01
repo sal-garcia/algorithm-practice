b1a7eeded12eaa61645f524bc1df879c1b59bb0d
sal <salgarcia940@gmail.com>
2023-01-31 22:20:47 -0800

    moved two sum function into new folder

--- algorithms/arrays&hashing/arrays&hashing.js
+++ algorithms/arrays&hashing/arrays&hashing.js
@@ -43,3 +43,18 @@ function isAnagram(s, t) {
 
 console.log(isAnagram('anagram', 'nagaram'), 'isAnagram (anagram tanagram)')
 // validAnagram
+
+//two sum
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) { //i+1 rather than simply 1 to avoid j and i from landing on the same value twice
+      if (nums[i] + nums[j] === target) {
+        return [i, j]//i and j only so that it grabs the indexes not the values
+      }
+    }
+  }
+}
+
+console.log(twoSum([1, 3, 2], 4), 'target is 4');
+console.log(twoSum([1, 3, 2], 5), 'targer is 5');
+//two sum
